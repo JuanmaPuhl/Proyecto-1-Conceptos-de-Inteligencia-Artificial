a94f2e2f7e71100ccf09326fe1daa7f170b02f4b
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2020-11-14 19:59:27 -0300

    ya me canse

--- Proyecto 2/main.pl
+++ Proyecto 2/main.pl
@@ -1,13 +1,14 @@
-:- dynamic ejemplo/2.
+:- dynamic ejemplo/3.
 construirAD(InputFile,OutputFile):- 
                                     \+checkEmpty(InputFile), 
                                     consult(InputFile), 
-                                    findall(X, ejemplo(_,X), L),
+                                    findall(X, ejemplo(_,_,X), L),
+                                    length(L,S),
+                                    S>0,
                                     sort(L,ListaFinal), 
                                     length(ListaFinal,Size), 
-                                    write(Size),
-                                    write(ListaFinal),
-                                    buscarDefault(ListaFinal,Default).
+                                    buscarDefault(ListaFinal,Default),!.
+construirAD(InputFile,OutputFile):- write("Ocurrio un error.").
 checkEmpty(File):-open(File,read,Str),isEmpty(Str).
 /*
 Tengo que hacer algun metodo para revisar cual es el predicado que mas veces aparece para el default
@@ -16,16 +17,25 @@ Para cada uno entonces debo guardar en una lista el par (Nombre,Cantidad) y una 
 Sort creo que los ordena.
 */
 
-buscarDefault(ListaInput, Default):-buscarApariciones(ListaInput,ListaOutput),write(ListaOutput),buscarMayor(ListaOutput,Default).
+buscarDefault(ListaInput, Default):-buscarApariciones(ListaInput,ListaOutput),buscarMayor(ListaOutput,Default),write("Default: "),write(Default).
 buscarApariciones(ListaInput,ListaOutput):-buscarAparicionesAux(ListaInput,[],ListaOutput).
 buscarAparicionesAux([],ListaIntermedia,ListaOutput):-append(ListaNueva,ListaIntermedia,ListaOutput),!.
 buscarAparicionesAux([H|T],ListaIntermedia,ListaOutput) :-
-                                                        findall(ID, ejemplo(ID,H),ListaAux),
-                                                        length(ListaAux,Cantidad),ListaNueva=[(H,Cantidad)],
+                                                        findall(ID, ejemplo(ID,_,H),ListaAux),
+                                                        length(ListaAux,Cantidad),ListaNueva=[(Cantidad,H)],
                                                         append(ListaIntermedia,ListaNueva,ListaNueva2),
                                                         buscarAparicionesAux(T,ListaNueva2,ListaOutput).
-buscarMayor(ListaOutput,Default):-sort(ListaOutput,ListaOrdenada),write(ListaOrdenada).
+/*
+En la lista final tengo todo ordenado, sin repeticiones, y el primero es el que mas apariciones tiene
+*/
+buscarMayor(ListaOutput,Default):-sort(ListaOutput,ListaOrdenada),reverse(ListaOrdenada,ListaFinal),obtenerMayor(ListaFinal,Default).
+obtenerMayor([(A,(B,C))|T],Default):- Default = C.
 
+/*
+Ahora tengo que generar el arbol de decision
+*/
+
+generarArbolDecision.
 
 isEmpty(Str):-at_end_of_stream(Str).
 
