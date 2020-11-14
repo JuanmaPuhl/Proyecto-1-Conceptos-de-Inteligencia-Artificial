da7ec3f26920f6deba021646663f6facd18e33fd
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2020-11-14 18:21:58 -0300

    empezando a buscar repeticiones

--- Proyecto 2/main.pl
+++ Proyecto 2/main.pl
@@ -1,9 +1,30 @@
 :- dynamic ejemplo/2.
-construirAD(InputFile,OutputFile):- \+checkEmpty(InputFile), consult(InputFile), findall(X, ejemplo(_,X), L), length(L,Size), write(Size),write(L).
-
+construirAD(InputFile,OutputFile):- 
+                                    \+checkEmpty(InputFile), 
+                                    consult(InputFile), 
+                                    findall(X, ejemplo(_,X), L),
+                                    sort(L,ListaFinal), 
+                                    length(ListaFinal,Size), 
+                                    write(Size),
+                                    write(ListaFinal),
+                                    buscarDefault(ListaFinal,Default).
 checkEmpty(File):-open(File,read,Str),isEmpty(Str).
+/*
+Tengo que hacer algun metodo para revisar cual es el predicado que mas veces aparece para el default
+Debo recorrer la lista final, llamando a findall contando todos los ids que aparezcan con el nombre indicado en la lista. 
+Para cada uno entonces debo guardar en una lista el par (Nombre,Cantidad) y una vez que termino entonces recorro y veo el mayor.
+Sort creo que los ordena.
+*/
 
-
+buscarDefault(ListaInput, Default):-buscarApariciones(ListaInput,ListaOutput),write(ListaOutput),buscarMayor(ListaOutput,Default).
+buscarApariciones(ListaInput,ListaOutput):-buscarAparicionesAux(ListaInput,[],ListaOutput).
+buscarAparicionesAux([],ListaIntermedia,ListaOutput):-append(ListaNueva,ListaIntermedia,ListaOutput),!.
+buscarAparicionesAux([H|T],ListaIntermedia,ListaOutput) :-
+                                                        findall(ID, ejemplo(ID,H),ListaAux),
+                                                        length(ListaAux,Cantidad),ListaNueva=[(H,Cantidad)],
+                                                        append(ListaIntermedia,ListaNueva,ListaNueva2),
+                                                        buscarAparicionesAux(T,ListaNueva2,ListaOutput).
+buscarMayor(ListaOutput,Default):-sort(ListaOutput,ListaOrdenada),write(ListaOrdenada).
 
 
 isEmpty(Str):-at_end_of_stream(Str).
