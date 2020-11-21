4f845c6c20c26cad63daef2b1cb1b80d2bf6ec78
Juan Manuel Puhl <43391912+JuanmaPuhl@users.noreply.github.com>
2020-11-21 18:46:45 -0300

    reacomodo codigo

--- Proyecto 2/main.pl
+++ Proyecto 2/main.pl
@@ -61,7 +61,6 @@ generarArbolDecisionShell(Default) :-
                                     writeln(ListaFinalAtributos),
                                     generarArbolDecision(ListaEjemplos,ListaFinalAtributos,Default).
 
-
 generarArbolDecision(ListaEjemplos,ListaAtributos,Default):-length(ListaEjemplos,Size),Size==0,writeln(Default).
 generarArbolDecision(ListaEjemplos,ListaAtributos,Default):-verificarIgualesShell(ListaEjemplos).
 generarArbolDecision(ListaEjemplos,ListaAtributos,Default):-writeln("Caso General").
@@ -81,28 +80,41 @@ searchAttribute([Attr|Tail],ListaEjemplos):-findall((Attr,Valor,Calificacion),(m
                     write("Atributo: "),
                     writeln(Attr),
                     buscarTotalAtributosPorClasificacion(ListaClasificacionSinRepetidos,Attr,ListaNueva),searchAttribute(Tail,ListaEjemplos).
-replaceP(_, _, [], []).
-replaceP(O, R, [O|T], [R|T2]) :- replaceP(O, R, T, T2).
-replaceP(O, R, [H|T], [H|T2]) :- dif(H,O), replaceP(O, R, T, T2).
 buscarTotalAtributosPorClasificacion([],Attr,Lista).
 buscarTotalAtributosPorClasificacion([Clasificacion|T],Attr,Lista):-findall(Value,member((_,Value,_),Lista),ListaValores),
                                                                     sort(ListaValores,ListaValoresSinRepetidos),
                                                                     buscarTotal(ListaValoresSinRepetidos,Lista),
-                                                                    write("Clasificacion: "),write(Clasificacion),
+                                                                    write("Clasificacion: "),
+                                                                    write(Clasificacion),
                                                                     buscarParcial(Clasificacion,ListaValoresSinRepetidos,Attr,Lista),
                                                                     buscarTotalAtributosPorClasificacion(T,Attr,Lista).
 buscarTotal([],Lista).
-buscarTotal([Valor|T],Lista):-findall(Valor,member((_,Valor,_),Lista),ListaNueva),length(ListaNueva,Size),write("Valor: "),write(Valor),write(" Total: "),writeln(Size),buscarTotal(T,Lista).
+buscarTotal([Valor|T],Lista):-
+                            findall(Valor,member((_,Valor,_),Lista),ListaNueva),
+                            length(ListaNueva,Size),
+                            write("Valor: "),
+                            write(Valor),
+                            write(" Total: "),
+                            writeln(Size),
+                            buscarTotal(T,Lista).
 buscarParcial(Clasificacion,[],Attr,Lista).
-buscarParcial(Clasificacion,[Valor|T],Attr,Lista):-findall(Valor,member((Attr,Valor,Clasificacion),Lista),ListaEncontrada),length(ListaEncontrada,Size),write(" Valor: "),write(Valor),write(" Cantidad: "),writeln(Size),buscarParcial(Clasificacion,T,Attr,Lista).
+buscarParcial(Clasificacion,[Valor|T],Attr,Lista):-
+                            findall(Valor,member((Attr,Valor,Clasificacion),Lista),ListaEncontrada),
+                            length(ListaEncontrada,Size),
+                            write(" Valor: "),
+                            write(Valor),
+                            write(" Cantidad: "),
+                            writeln(Size),
+                            buscarParcial(Clasificacion,T,Attr,Lista).
+
+/*Utilidades*/
 isEmpty(Str):-at_end_of_stream(Str).
-
 leer(InputFile) :- open(InputFile, read, Str), read_file(Str,Lines), close(Str),!.
-
 read_file(Stream,[]) :- at_end_of_stream(Stream).
-
 read_file(Stream,[X|L]) :- \+ at_end_of_stream(Stream), read(Stream,X), call(X), read_file(Stream,L).
-
 verificarIgualesShell([(_,_,(_,E))|T]):-verificarIguales(T,E).
 verificarIguales([],E).
 verificarIguales([(_,_,(_,Calificacion))|T],E):-Calificacion==E,verificarIguales(T,E).
+replaceP(_, _, [], []).
+replaceP(O, R, [O|T], [R|T2]) :- replaceP(O, R, T, T2).
+replaceP(O, R, [H|T], [H|T2]) :- dif(H,O), replaceP(O, R, T, T2).
